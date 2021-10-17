b57f715d506a60a610c07a80e4291e55275b58c8
Tom Fan <tomboxfan@hotmail.com>
2021-10-17 14:06:46 +0800

    homework

---
+++ homework/wu_yantong.py
@@ -0,0 +1,82 @@
+# PYTHON
+
+space = ' ' * 10
+space_5x = ' ' * 5 + 'x' * 5
+space_2x = ' ' * 5 + 'x' + ' ' * 3 + 'x'
+space_left = ' ' * 5 + 'x'
+space_right = ' ' * 9 + 'x'
+space_middle = ' ' * 7 + 'x'
+
+# P
+print(space)
+print(space)
+print(space)
+print(space)
+print(space_5x)
+print(space_2x)
+print(space_2x)
+print(space_5x)
+print(space_left)
+print(space_left)
+print(space_left)
+
+# Y
+print(space)
+print(space)
+print(space)
+print(space_2x)
+print(space_2x)
+print(space_2x)
+print(space_5x)
+print(space_right)
+print(space_right)
+print(space_5x)
+
+# T
+print(space)
+print(space)
+print(space)
+print(space_5x)
+print(space_middle)
+print(space_middle)
+print(space_middle)
+print(space_middle)
+print(space_middle)
+print(space_middle)
+
+# H
+print(space)
+print(space)
+print(space)
+print(space_2x)
+print(space_2x)
+print(space_2x)
+print(space_5x)
+print(space_2x)
+print(space_2x)
+print(space_2x)
+
+# O
+print(space)
+print(space)
+print(space)
+print(space_5x)
+print(space_2x)
+print(space_2x)
+print(space_2x)
+print(space_2x)
+print(space_2x)
+print(space_5x)
+
+# N
+print(space)
+print(space)
+print(space)
+print(space_5x)
+print(space_2x)
+print(space_2x)
+print(space_2x)
+print(space_2x)
+print(space_2x)
+print(space_2x)
+
