39a5f445ddcaba1fa5c2ae8849948e4bc2af9040
Tom Fan <tomboxfan@hotmail.com>
2021-11-20 10:19:43 +0800

    lesson 7

--- program1/python_0024_practice_score_rating.py
+++ program1/python_0024_practice_score_rating.py
@@ -5,3 +5,11 @@ if it is equals to 60 or above, your program prints 'B'
 other score, print 'C'
 '''
 
+score = int(input("Your score:"))
+
+if score >= 90:
+    print('A')
+elif score >= 60:
+    print('B')
+else:
+    print('C')
