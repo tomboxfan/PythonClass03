9b9d4cbe0b068617cc9ad8a32a781c8d67344558
Tom Fan <tomboxfan@hotmail.com>
2022-02-26 10:53:02 +0800

    lesson 21

---
+++ program2/python_0083_practice_data_structure_list_4_seasons.py
@@ -0,0 +1,9 @@
+# You are given 4 seasons list as below
+spring = [3,4,5]
+summer = [6,7,8]
+autumn = [9,10,11]
+winter = [12,1,2]
+
+# You need to ask user to input a number between 1 - 12
+# Then you tell the user which season it belongs to.
+
